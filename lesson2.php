cd808dd0981ef12271d836c2a127f8831248831e
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-06 09:17:28 +0530

    check case sensitivity of PHP

---
+++ lesson2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP is case sensitive</title>
+</head>
+<body>
+    <?php
+    $capital = 67;
+    print("variable capital is $capital<br>");
+    print("variable CaPiTal is $CaPiTal") //This one displays an error because variables are case sensitive in PHP
+    ?>
+</body>
+</html>
