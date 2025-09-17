31a46213cf129562faded2ba77512234c86b71f9
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-17 08:58:41 +0530

    if-else-elseif statements

--- lesson7(Control statements).php
+++ lesson7(Control statements).php
@@ -8,6 +8,16 @@ if ($user == "John"){
     print "You are not John";
 }
 
+echo "<br>";
 
+$foo = 10;
+
+if ($foo == 0){
+    echo "The variable foo is equal to 0";
+} elseif (($foo > 0) && ($foo <= 5)){
+    echo "The variable foo is between 1 to 5";
+} else {
+    echo "The variable foo is equal to $foo";
+}
 
 ?>
