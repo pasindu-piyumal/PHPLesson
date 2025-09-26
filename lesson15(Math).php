53b3cb1a4a19522dc6a4e6c98199f845723a46b9
pasindu_piyumal <22cds0423@ms.sab.ac.lk>
2025-09-26 18:06:14 +0530

    creating maths functions using php

---
+++ lesson15(Math).php
@@ -0,0 +1,19 @@
+<?php
+
+$sum = 10 + 5;
+$different = 20 - 8;
+$product = 6 * 4;
+$quotient = 50 / 2;
+
+echo "<p>Sum: $sum</p>";
+echo "<p>different: $different</p>";
+echo "<p>Product: $product</p>";
+echo "<p>Quotient: $quotient</p>";
+
+$reminder = 15 % 7;
+$power = 2 ** 4;
+
+echo "<p>Reminders: $reminder</p>";
+echo "<p>2 to the power 4: $power</p>";
+
+?>
